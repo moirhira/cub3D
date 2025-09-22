523d0c8c62acde45d054df6a74c88479e6586367
Mohamed Irhirallah <moirhira@c2r9p12.1337.ma>
2025-09-22 15:34:44 +0100

    setup the project stucture

---
+++ src/main.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moirhira <moirhira@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 14:53:51 by moirhira          #+#    #+#             */
+/*   Updated: 2025/09/21 15:08:01 by moirhira         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3d.h"
+
+// int	read_map(t_game *gamedata, char *filedata)
+// {
+//     int	i;
+// 	int	fd;
+
+// 	fd = open(file, O_RDONLY);
+// 	if (fd == -1)
+// 		return (ft_printf("Error\nopening file\n"), 0);
+
+// }
+
+
+// void	init_data(t_game *game)
+// {
+    
+// }
+
+// int	init_game(t_game **gamedata, char *file)
+// {
+// 	*gamedata = malloc(sizeof(t_game));
+// 	if (!*gamedata)
+// 		return (printf("Error\nmalloc\n"),0);
+// 	(*gamedata)->map = malloc(sizeof(t_map));
+//     if (!*gamedata)
+//     {
+//         free(*gamedata)
+// 		return (printf("Error\nmalloc\n"),0);
+//     }
+//     if (!read_map((*gamedata)->mapdata, file))
+// 	{
+// 		free((*gamedata)->map);
+// 		free(*gamedata);
+// 		return (0);
+// 	}
+// 	init_data(*gamedata)
+// 	return (1);
+// }
+
+int main(int ac, char **av)
+{
+    // t_game *game;
+    // if (ac != 2)
+	// 	return (ft_printf("Error\nUsage: ./cub3D path/<filename>\n"), 1);
+    // if (!init_game(&game, av[1]))
+    //     return (1);
+    // game->mlx = mlx_init();
+    // if (!game->mlx)
+    //     return (ft_printf("Error\nmlx_init fail!\n"), 0);
+    // game->win = 
+    printf("hhe");
+    return (0);
+}
